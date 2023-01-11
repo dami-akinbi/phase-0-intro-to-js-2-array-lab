e45845fb67723be7b4c8c77e9a1825197ae2c9cd
damilolaakinbi <akinbidamilola074@gmail.com>
2023-01-11 15:53:17 +0100

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  let catsCopy = cats.slice();
+  catsCopy.push(name);
+  return catsCopy;
+}
+
+function prependCat(name) {
+  let catsCopy = [...cats];
+  catsCopy.unshift(name);
+  return catsCopy;
+}
+
+function removeLastCat() {
+  let catsCopy = [...cats];
+  catsCopy.pop();
+  return catsCopy;
+}
+
+function removeFirstCat() {
+  let catsCopy = cats.slice();
+  catsCopy.shift();
+  return catsCopy;
+}
